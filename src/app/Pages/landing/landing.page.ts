4e36984d9ab72709d565bcec13cc87af85cec0de
Muenda Khuthadzo <muendakhuthadzo@gmail.com>
2019-11-25 19:07:28 +0200

    almost done with landing page

--- src/app/Pages/landing/landing.page.ts
+++ src/app/Pages/landing/landing.page.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.page.scss'],
 })
 export class LandingPage implements OnInit {
+  thatobabe;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
