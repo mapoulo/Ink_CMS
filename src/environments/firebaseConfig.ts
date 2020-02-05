478f76305c8b46a5b9900baca0c954c9c3d6db62
Muenda Khuthadzo <muendakhuthadzo@gmail.com>
2020-02-05 12:23:11 +0200

    push

--- src/environments/firebaseConfig.ts
+++ src/environments/firebaseConfig.ts
@@ -1,17 +1,17 @@
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyBnbeeSgLhqDOOPlclSaIpDVdKYd3_Vq5Q",
-//   authDomain: "tattoo-964b7.firebaseapp.com",
-//   databaseURL: "https://tattoo-964b7.firebaseio.com",
-//   projectId: "tattoo-964b7",
-//   storageBucket: "tattoo-964b7.appspot.com",
-//   messagingSenderId: "396095430599",
-//   appId: "1:396095430599:web:3675e9c40eb99256d303e2",
-//   measurementId: "G-8TQ9T2QMHS",
-//   vapidKey: 'BCGequqI1YHWjkM6s4cJ-RVbGV8pW8f-fjeGjNERQTbPGBAZdpYcrT5DgmTxvXtN_5fIPbeBf2y9tVS6xFR9ukU'
-//   };
+export const firebaseConfig = {
+  apiKey: "AIzaSyBnbeeSgLhqDOOPlclSaIpDVdKYd3_Vq5Q",
+  authDomain: "tattoo-964b7.firebaseapp.com",
+  databaseURL: "https://tattoo-964b7.firebaseio.com",
+  projectId: "tattoo-964b7",
+  storageBucket: "tattoo-964b7.appspot.com",
+  messagingSenderId: "396095430599",
+  appId: "1:396095430599:web:3675e9c40eb99256d303e2",
+  measurementId: "G-8TQ9T2QMHS",
+  vapidKey: 'BCGequqI1YHWjkM6s4cJ-RVbGV8pW8f-fjeGjNERQTbPGBAZdpYcrT5DgmTxvXtN_5fIPbeBf2y9tVS6xFR9ukU'
+  };
 
 
-  export const firebaseConfig = {
+/*   export const firebaseConfig = {
     apiKey: "AIzaSyBnbeeSgLhqDOOPlclSaIpDVdKYd3_Vq5Q",
     authDomain: "tattoo-964b7.firebaseapp.com",
     databaseURL: "https://tattoo-964b7.firebaseio.com",
@@ -20,4 +20,4 @@
     messagingSenderId: "396095430599",
     appId: "1:396095430599:web:3675e9c40eb99256d303e2",
     measurementId: "G-8TQ9T2QMHS"
-  };
+  }; */
